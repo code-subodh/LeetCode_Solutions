45d00c46cb503a3e53dd4784808dd04ee0899738
Subodh Kumar Singh <111258218+code-subodh@users.noreply.github.com>
2026-08-18 08:47:59 +0530

    Time: 0 ms (100%), Space: 31.3 MB (81.82%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n = arr.size();
+        int s = 0;
+        int e = n - 1;
+        int mid = s + (e - s) / 2;
+        while (s <= e) {
+
+            if (target == arr[mid]) {
+                return mid;
+            }
+            if (target > arr[mid]) {
+                // right mein jao
+                s = mid + 1;
+            } else if (target < arr[mid]) {
+                // left mein jao
+                e = mid - 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        // iska matlab element not found..
+        return -1;
+    }
+};
