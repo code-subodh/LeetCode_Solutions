24e1658f50761ae49b9ba053bb1399f0c734500e
Subodh Kumar Singh <111258218+code-subodh@users.noreply.github.com>
2026-09-03 23:32:49 +0530

    Time: 3 ms (28.69%), Space: 28.4 MB (14.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // unordered_map<int, int> freq;
-        // for (int i = 0; i < nums.size(); i++) {
-        //     freq[nums[i]]++;
-        // }
+        unordered_map<int, int> freq;
+        int n = nums.size();
+        int min = (n / 2) + 1;
+        int ans = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+            if (freq[nums[i]] == min) {
+                ans = nums[i];
+                break;
+            }
+        }
+        return ans;
         // int max_count = INT_MIN;
         // int ans = INT_MIN;
         // for (int i = 0; i < nums.size(); i++) {
@@ -15,19 +23,19 @@ public:
         // }
         // return ans;
 
-        // Optimal solution
-        int condidate = 0;
-        int count = 0;
-        for (int num : nums) {
-            if (count == 0) {
-                condidate = num;
-            }
-            if (num == condidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-        return condidate;
+        // // Optimal solution
+        // int condidate = 0;
+        // int count = 0;
+        // for (int num : nums) {
+        //     if (count == 0) {
+        //         condidate = num;
+        //     }
+        //     if (num == condidate) {
+        //         count++;
+        //     } else {
+        //         count--;
+        //     }
+        // }
+        // return condidate;
     }
 };
